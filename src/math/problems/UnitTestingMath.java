56eaa0002e62b90172727695156618671446124e
marufn17 <marufn17@gmail.com>
2018-12-23 22:52:27 -0500

    MakePyramid done

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -10,35 +10,5 @@ public class UnitTestingMath {
         //Apply Unit testing into each classes and methods in this package.
 
 
-        try {//unit testing for Factorial
-            Assert.assertEquals(Factorial.factorial(5), 120);
-            System.out.println("Unit testing passed for factorial");
-        }catch (AssertionError ar){
-            System.out.println("Unit testing failed for factorial");
-        }
-
-        try {//unit testing for Fibonacci
-            ArrayList<Integer> expectedArray = new ArrayList<>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986));
-            Assert.assertEquals(Fibonacci.arrayFibo(), expectedArray);
-            System.out.println("Unit testing passed for fibonacci");
-        }catch (AssertionError as){
-            System.out.println("Unit testing failed for fibonacci");
-        }
-
-        try {//unit testing for FindLowestDifference
-            Assert.assertEquals(FindLowestDifference.lowestDifference(),13);
-            System.out.println("Unit testing passed for FindLowestDifference");
-        }catch (AssertionError as){
-            System.out.println("Unit testing failed for FindLowestDifference");
-        }
-
-        try{
-            Assert.assertEquals(FindMissingNumber.missingNumber(),9);
-            System.out.println("Unit testing passed for FindMissingNumber");
-        }catch (AssertionError as){
-            System.out.println("Unit testing failed for FindMissingNumber");
-        }
-
-
     }
 }
