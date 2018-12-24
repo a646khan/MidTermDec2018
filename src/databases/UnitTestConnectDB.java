4079c5d3ece93df383fa02ff6893ed9a0110c0b3
marufn17 <marufn17@gmail.com>
2018-12-24 00:20:06 -0500

    UnitTestConnectDB done

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -1,14 +1,23 @@
 package databases;
 
-import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class UnitTestConnectDB {
-    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+    @Test
+    public void connectionTest() {
         ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-        ConnectToSqlDB.connectToSqlDatabase();
+        try {
+            ConnectToSqlDB.connectToSqlDatabase();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
