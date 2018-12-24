085775cae2fe80df9c7592542f0fc3a4c4da984e
marufn17 <marufn17@gmail.com>
2018-12-23 21:12:55 -0500

    Fibonacci updated with unit testing

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -1,8 +1,14 @@
 package databases;
 
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class UnitTestConnectDB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+        ConnectToSqlDB.connectToSqlDatabase();
 
     }
 }
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -9,23 +9,29 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
+
+        System.out.println(arrayFibo());
+    }
+
+
+
+    public static ArrayList<Integer> arrayFibo(){
+
         int fab[] = new int[40];
         ArrayList<Integer> ar = new ArrayList<>();
-
 
         for (int m = 0; m < 40; m++) {
             if (m < 2) {
                 fab[m] = m;
+                ar.add(fab[m]);
 
             } else {
                 fab[m] = fab[m - 2] + fab[m - 1];
                 ar.add(fab[m]);
             }
         }
-        System.out.println(Arrays.toString(fab));
-        System.out.println(ar);
 
-        List<String> a = new ArrayList<String>();
-
+    return ar;
     }
+
 }
