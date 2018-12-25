e3df3cfd360c7d6e879144300ccd4d513fde5ee5
marufn17 <marufn17@gmail.com>
2018-12-24 19:10:07 -0500

    Largest and Duplicaet word done

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -10,17 +10,37 @@ public class DetermineLargestWord {
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-        String s="Human brain is a biological learning machine";
+        String s ="Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
-        //implement
+
+        System.out.println("Word length from the given sentence is "+wordNLength);
+        System.out.println("Largest word from the given sentence is "+largestWord(s));
 
     }
 
-    public static Map<Integer, String> findTheLargestWord(String wordGiven){
+    public static Map<Integer, String> findTheLargestWord(String givenWord){
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
-        //implement
 
+        String[] sep = givenWord.split(" ");
+
+        for (int i = 0; i < sep.length; i++) {
+            map.put(sep[i].length(), sep[i]);
+        }
         return map;
+    }
+
+    public static String largestWord(String givenWord) {
+        DetermineLargestWord dt = new DetermineLargestWord();
+        Map<Integer, String> wordNLength = dt.findTheLargestWord(givenWord);
+        int largest = 0;
+        String largeValue = "";
+        for (Map.Entry<Integer, String> checkLength : wordNLength.entrySet()) {
+            if (largest < checkLength.getKey()) {
+                largest = checkLength.getKey();
+                largeValue = checkLength.getValue();
+            }
+        }
+        return largeValue;
     }
 }
